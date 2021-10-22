6db442c794293ca12d24b54476c05ac7ecb39427
Amek <ameeeekWork@gmail.com>
2021-10-22 18:44:58 +0800

    menambahkan hover text pada navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,11 +4,11 @@ const navBar = () => {
     return (
         <div className="container flex justify-between">
             <div className="logo-container">
-                <i>encourag-er</i>
+                <p>encourag-er</p>
             </div>
             <div className="navbar-links flex text-lg">
-                <Link  to="/about" class="mx-3 hover:bg-black hover:text-white ease-linear duration-150">Home</Link>
-                <Link  to="/sign-up" class="mx-3">Sign Up</Link>
+                <Link  to="/about" class="mx-3 hover:text-white hover:bg-indigo-600 ease-linear duration-150">Home</Link>
+                <Link  to="/sign-up" class="mx-3 hover:text-white hover:bg-indigo-600 ease-linear duration-150">Sign Up</Link>
                 <Link  to="/Login" class="mx-3">Login</Link>
             </div>
         </div>
